374314bd8e6479cec195869934e40f5337204152
humam-hossain <humam.hossain.e@gmail.com>
2023-03-02 01:50:32 +0600

    mapbox not working properly

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import React, { useState } from "react";
+import ReactMapGl from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
 const index = () => {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    const [viewport, setViewport] = useState({
+        latitude: 23.7640432,
+        longitude: 90.4240439,
+        width: "100%",
+        height: "calc(100vh-64px)",
+        zoom: 10,
+    });
+
+    return (
+        <>
+            <ReactMapGl
+                {...viewport}
+                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
+            >
+                markers here
+            </ReactMapGl>
+        </>
+    );
 };
 
 export default index;
